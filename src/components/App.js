a00fd1107c8eac1f4f38783a4b8966c0b4f647f8
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-02-10 17:51:35 +0000

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Time Spent on Page: {seconds} seconds</h1>
     </div>
-  )
+  );
 }
 
 export default App
