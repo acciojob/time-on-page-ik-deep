76968c70aa1036778401ec8bbb756fcc2bb75826
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-02-10 18:03:22 +0000

    paased

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div>
-      <p>You've been on this page for {seconds} seconds</p>
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
   );
 }
